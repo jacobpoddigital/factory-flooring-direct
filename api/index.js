98f10e175195a8ab9719539d19c955247b9b133e
jacobpoddigital <jacob@poddigital.co.uk>
2026-09-22 09:11:02 +0100

    Add Express API handler with static file serving

--- api/index.js
+++ api/index.js
@@ -3,26 +3,14 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files
 app.use(express.static(path.join(__dirname, '..')));
 
-// Handle all routes to serve files with query string support
 app.get('*', (req, res) => {
-  // Strip query string from URL
-  const urlWithoutQuery = req.path === '/' ? '/index.html' : req.path;
-  const filePath = path.join(__dirname, '..', urlWithoutQuery);
-
-  // Security: prevent directory traversal
-  const resolvedPath = path.resolve(filePath);
-  const rootPath = path.resolve(path.join(__dirname, '..'));
-  if (!resolvedPath.startsWith(rootPath)) {
-    return res.status(403).send('Forbidden');
-  }
+  const urlPath = req.path === '/' ? '/index.html' : req.path;
+  const filePath = path.join(__dirname, '..', urlPath);
 
   res.sendFile(filePath, (err) => {
-    if (err) {
-      res.status(404).send('Not Found');
-    }
+    if (err) res.status(404).send('Not Found');
   });
 });
 
